bda20c348c6db891bf834738418bb9916026b6eb
Helistinify <Heikki.vayrynen92@gmail.com>
2018-02-21 12:40:10 +0200

    t: Peetu, Teemu, Aleksi, Micke☻
    Crouchi, teleportti, physics material pallolle, hierarkiaa

---
+++ OculusVRTesti/Oculus VR testi/Assets/OmatAssetit/Scripts/Crouch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour {
+    
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0f, 1.2f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel")) // When the button is pressed down
+        {
+            SetCrouch(true);
+        }
+        if (Input.GetButtonUp("Cancel"))// When the button is released
+        {
+            SetCrouch(false);
+        }
+    }
+
+    void SetCrouch(bool b)
+    {
+        if (b)
+            transform.position -= offset;
+        else
+            transform.position += offset;
+    }
+}
---
+++ OculusVRTesti/Oculus VR testi/Assets/OmatAssetit/Scripts/Teleporting.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporting : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject Controller;
+    public GameObject RHP_Object;
+    Renderer RHP_Render;
+    bool ShowIndicator;
+    bool CanTeleport;
+    Vector3 TPosition;
+
+	void Start () {
+        //Take reference to controller
+        Controller = gameObject;
+        RHP_Render = RHP_Object.GetComponent<Renderer>();
+        CanTeleport = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit")) // When the button is pressed down
+        {
+            ShowIndicator = true;
+        }
+        if (Input.GetButtonUp("Submit"))// When the button is released
+        {
+            ShowIndicator = false;
+            // Move player object to wherever the raycast hit point is at
+            if (CanTeleport)
+            {
+                transform.parent.parent.parent.position = TPosition;
+                CanTeleport = false;
+                RendererEnabled(CanTeleport);
+            }
+        }
+
+    }
+
+    void FixedUpdate () {
+		if (ShowIndicator)
+        {
+            // Raycast forward from Controller
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                // Move an object to the raycast hit point
+                if (hit.transform.tag == "Ground")
+                {
+                    RHP_Object.transform.position = hit.point;
+                    TPosition = hit.point;
+                    CanTeleport = true;
+                }
+                else
+                { 
+                    CanTeleport = false;
+                }
+                RendererEnabled(CanTeleport);
+            }
+        }
+    }
+
+    void RendererEnabled(bool b)
+    {
+        RHP_Render.enabled = b; 
+    }
+}
